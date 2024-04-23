411d8948672a4411e6c8e780d8c7fa43cbf59e95
KevinSaG <kvnsgto@gmail.com>
2024-04-23 14:04:26 -0500

    Add code to save data

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    full_name = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.full_name
--- home/views.py
+++ home/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views import View
+from .models import User
 
 class HomeView (View):
     def get(self, request):
         context = {}
         return render(request, 'home//home.html', context=context)
+    
+    def post(self, request):
+        # Extract data
+        full_name = request.POST.get('full_name', '')
+        company_name = request.POST.get('company', '')
+        phone = request.POST.get('phone', '')
+        email = request.POST.get('email', '')
+        
+        # Guardar en base de datos
+        user_entry = User(full_name=full_name, company_name=company_name, phone=phone, email=email)
+        user_entry.save()
+        
+        return redirect('home')
